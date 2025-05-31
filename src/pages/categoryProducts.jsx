ed229c53986514bd3bf5cabcdb0717a9b3f1ae0d
Agugbue Ikenna Nzubechi <iamagugbueikenna@gmail.com>
2025-05-31 02:53:21 +0100

    Update categoryProducts.jsx

--- src/pages/categoryProducts.jsx
+++ src/pages/categoryProducts.jsx
@@ -3,15 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-<<<<<<< HEAD
-=======
-const baseURL =
-  import.meta?.env?.VITE_API_URL || "https://project-1-207l.onrender.com";
 
-const api = axios.create({
-  baseURL,
-});
->>>>>>> bfd326b (new commit)
 
 const CategoryProducts = () => {
   const { categoryName } = useParams();
