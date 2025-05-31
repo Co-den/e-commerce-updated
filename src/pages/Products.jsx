656d0ec890fcc4445ada3467d358a5cf35c3405e
Agugbue Ikenna Nzubechi <iamagugbueikenna@gmail.com>
2025-05-31 02:52:48 +0100

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,16 +4,7 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
 
-<<<<<<< HEAD
-=======
-const baseURL =
-  import.meta?.env?.VITE_API_URL || "https://project-1-207l.onrender.com";
 
-const api = axios.create({
-  baseURL,
-});
-
->>>>>>> bfd326b (new commit)
 const Product = () => {
   const [products, setProducts] = useState([]);
 
