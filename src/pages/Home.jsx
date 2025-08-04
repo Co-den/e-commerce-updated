c677b709b206ea46e44ae1d2c9af74d29afebca5
Agugbue Ikenna Nzubechi <iamagugbueikenna@gmail.com>
2025-08-04 19:28:03 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -113,7 +113,7 @@ const Home = () => {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className="relative pt-16 pb-32 bg-green from-green-400 to-green-500 overflow-hidden"
+        className="relative pt-16 pb-32 bg-green-700 from-green-400 to-green-700 overflow-hidden"
       >
     
         {/* Centered Content */}
