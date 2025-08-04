7b0ce26e1de718ecc1a3a10545e48343559776cc
Agugbue Ikenna Nzubechi <iamagugbueikenna@gmail.com>
2025-08-04 14:59:19 +0100

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,10 +4,6 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/main
 const Product = () => {
   const [products, setProducts] = useState([]);
 
@@ -20,7 +16,7 @@ const Product = () => {
 
   return (
     <div className="container mx-auto p-4 bg-white min-h-screen">
-      <h2 className="text-3xl font-bold text-blue mb-6">Products</h2>
+      <h2 className="text-3xl font-bold text-green mb-6">Products</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product, index) => (
           <motion.div
@@ -29,14 +25,14 @@ const Product = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
             whileHover={{ scale: 1.03 }}
-            className="bg-white border border-green p-4 rounded-lg shadow-lg hover:text- hover:bg-blue  transition-all"
+            className="bg-white border border-green p-4 rounded-lg shadow-lg hover:bg-yellow transition-all"
           >
             <img
               src={product.image}
               alt={product.name}
               className="w-full h-48 object-cover rounded-lg mb-4 border-2 border-orange"
             />
-            <h3 className="text-blue text-sm font-semibold">{product.name}</h3>
+            <h3 className="text-orange text-xl font-semibold">{product.name}</h3>
             <p className="text-black text-lg mt-2">${product.price}</p>
             <Link
               to={`/product/${product.id}`}
